76c3e1ce69d630769c25d41a7f0c472bb289dfb0
Mustafa-wr <mradwan@student.42abudhabi.ae>
2025-04-27 15:48:56 +0400

    NSG for frontend and backend has been created

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
 provider "azurerm" {
   features {}
 }
+
 variable "resource_group_name" {
   description = "The name of the resource group"
   type        = string
@@ -17,13 +18,13 @@ variable "admin_username" {
   description = "The admin username for the virtual machine"
   type        = string
   default     = "adminuser"
-  
 }
 
 variable "admin_password" {
   description = "The admin password for the virtual machine"
   type        = string
   default     = "P@ssw0rd1234!"
+  sensitive   = true
 }
 
 variable "ssh_public_key" {
@@ -33,6 +34,12 @@ variable "ssh_public_key" {
 }
 
 variable "vm_network_name" {
+  description = "The name of the virtual network"
+  type        = string
+  default     = "mine-vnet"
+}
+
+variable "vm_nic_name" {
   description = "The name of the network interface for the virtual machine"
   type        = string
   default     = "mine-nic"
@@ -42,14 +49,7 @@ variable "ssh_private_key_path" {
   description = "The path to the SSH private key for the virtual machine"
   type        = string
   default     = "~/.ssh/id_rsa"
-  sensitive = true
-}
-
-data "azurerm_platform_image" "mine" {
-  location            = azurerm_resource_group.rg.location
-  publisher           = "debian"
-  offer              = "debian-11"
-  sku                = "11"
+  sensitive   = true
 }
 
 resource "azurerm_resource_group" "rg" {
@@ -61,16 +61,102 @@ resource "azurerm_virtual_network" "mine" {
   name                = var.vm_network_name
   address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.rg.location
-  resource_group_name = var.resource_group_name
+  resource_group_name = azurerm_resource_group.rg.name
 }
-
-resource "azurerm_subnet" "mine" {
-  name                 = "internal"
+resource "azurerm_subnet" "frontend" {
+  name                 = "frontend-subnet"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.mine.name
-  address_prefixes     = cidrsubnets(azurerm_virtual_network.mine.address_space[0], 8, 1)
+  address_prefixes     = ["10.0.1.0/24"]
 }
 
+resource "azurerm_subnet" "backend" {
+  name                 = "backend-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.mine.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_network_security_group" "frontend_nsg" {
+  name                = "frontend-nsg"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_network_security_rule" "allow_http" {
+  name                        = "allow-http"
+  priority                    = 100
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "80"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.rg.name
+  network_security_group_name = azurerm_network_security_group.frontend_nsg.name
+}
+
+resource "azurerm_network_security_rule" "allow_https" {
+  name                        = "allow-https"
+  priority                    = 110
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "443"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.rg.name
+  network_security_group_name = azurerm_network_security_group.frontend_nsg.name
+}
+
+resource "azurerm_network_security_group" "backend_nsg" {
+  name                = "backend-nsg"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_network_security_rule" "allow_frontend_only" {
+  name                        = "allow-frontend-only"
+  priority                    = 100
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "*"  # Allow all protocols
+  source_port_range           = "*"
+  destination_port_range      = "*"  # Allow all ports
+  source_address_prefix       = azurerm_subnet.frontend.address_prefixes[0]
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.rg.name
+  network_security_group_name = azurerm_network_security_group.backend_nsg.name
+}
+
+resource "azurerm_network_security_rule" "deny_all_inbound" {
+  name                        = "deny-all-inbound"
+  priority                    = 4000
+  direction                   = "Inbound"
+  access                      = "Deny"
+  protocol                    = "*"
+  source_port_range           = "*"
+  destination_port_range      = "*"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.rg.name
+  network_security_group_name = azurerm_network_security_group.backend_nsg.name
+}
+
+# Associate NSGs with corresponding Subnets
+resource "azurerm_subnet_network_security_group_association" "frontend_nsg_association" {
+  subnet_id                 = azurerm_subnet.frontend.id
+  network_security_group_id = azurerm_network_security_group.frontend_nsg.id
+}
+
+resource "azurerm_subnet_network_security_group_association" "backend_nsg_association" {
+  subnet_id                 = azurerm_subnet.backend.id
+  network_security_group_id = azurerm_network_security_group.backend_nsg.id
+}
+
+# Public IP for VM
 resource "azurerm_public_ip" "mine" {
   name                = "mine-pip"
   location            = azurerm_resource_group.rg.location
@@ -78,20 +164,21 @@ resource "azurerm_public_ip" "mine" {
   allocation_method   = "Dynamic"
 }
 
-
+# Network interface for VM - placed in the frontend subnet
 resource "azurerm_network_interface" "mine" {
-  name                = var.vm_network_name
+  name                = var.vm_nic_name
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 
   ip_configuration {
     name                          = "internal"
-    subnet_id                     = azurerm_subnet.mine.id
+    subnet_id                     = azurerm_subnet.frontend.id  # VM is in frontend subnet
     private_ip_address_allocation = "Dynamic"
-	public_ip_address_id         = azurerm_public_ip.mine.id
+    public_ip_address_id          = azurerm_public_ip.mine.id
   }
 }
 
+# VM Module (assuming your module is correctly set up)
 module "vm" {
   source               = "./modules/vm"
   location             = var.location
@@ -101,6 +188,7 @@ module "vm" {
   network_interface_id = azurerm_network_interface.mine.id
 }
 
+# Configuration for VM after creation
 resource "null_resource" "configure_vm" {
   depends_on = [module.vm.linux_virtual_machine]
 
@@ -119,5 +207,3 @@ resource "null_resource" "configure_vm" {
     }
   }
 }
-
-
